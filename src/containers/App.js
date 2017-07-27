2c0db2cd2138625a1c8fc32211621e99197e3a88
danikfedirko <danylo.fedirko@gmail.com>
2017-07-27 17:16:30 +0300

    build25

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 import routes from '../routes'
 import Main from './Main'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const App = React.createClass({
+  childContextTypes:{
+  muiTheme: React.PropTypes.object.isRequired,
+},
+getChildContext() {
+  return {muiTheme: getMuiTheme()};
+},
   render () {
     return (
       <Main>
@@ -12,5 +19,6 @@ const App = React.createClass({
     )
   }
 })
+
 
 export default App
